32d811885e8348c6135337085140d097f0154456
Langhere <argildamiis@gmail.com>
2024-08-25 23:57:03 +0800

    chall-jawa-update

--- pwn/jawa/config/run_challenge.sh
+++ pwn/jawa/config/run_challenge.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
+#!/bin/bash
+
+# Set JAVA_HOME and PATH in the script
+export JAVA_HOME=/opt/jdk-22
+export PATH=${JAVA_HOME}/bin:$PATH
+
+# Change directory and run the Java application
 cd /home/ctf && java chall
+
